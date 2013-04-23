2eecd49ce87f3a46c840a4fbbdb9f95aa996e103
Joonas Rouhiainen <Joonas.Rouhiainen@solita.fi>
2013-04-23 13:51:17 +0300

    Replace vars with scoped vals.

--- src/test/scala/com/github/joonasrouhiainen/joogo/BoardSpec.scala
+++ src/test/scala/com/github/joonasrouhiainen/joogo/BoardSpec.scala
@@ -4,12 +4,6 @@ import org.specs2.mutable._
 
 class BoardSpec extends Specification {
 
-  var board: Board = _
-
-  step {
-    board = new Board(9, 7)
-  }
-
   "A new 0x0 board" should {
 
     "be disallowed" in {
@@ -19,6 +13,8 @@ class BoardSpec extends Specification {
   }
 
   "A new 9x7 board" should {
+
+    val board = new Board(9, 7)
 
     "be empty when created" in {
       board.toString must_== "+++++++++\n" * 7
@@ -56,12 +52,15 @@ class BoardSpec extends Specification {
 
   "Placing a black stone on an empty board" should {
 
+    val emptyBoard = new Board(9, 7)
+    val board      = emptyBoard.place(Black, 1, 2)
+
     "make the stone retrievable" in {
-      board.place(Black, 1, 2).get(1, 2).get must_== Black
+      board.get(1, 2).get must_== Black
     }
 
     "make the stone appear at the right place" in {
-      board.place(Black, 1, 2).toString must_==
+      board.toString must_==
       "+++++++++\n" +
       "b++++++++\n" +
       "+++++++++\n" +
@@ -72,12 +71,11 @@ class BoardSpec extends Specification {
     }
 
     "leave the original board state untouched" in {
-      board.place(Black, 1, 2)
-      board.get(1, 2).isEmpty must beTrue
+      emptyBoard.get(1, 2).isEmpty must beTrue
     }
 
     "make it white's turn" in {
-      board.place(Black, 1, 2).whoseTurn must_== White
+      board.whoseTurn must_== White
     }
 
   }
