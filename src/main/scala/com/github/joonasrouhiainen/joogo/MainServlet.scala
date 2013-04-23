b0d7324c84b5583ba5ad05a2bac37c01d8b46ccf
Joonas Rouhiainen <Joonas.Rouhiainen@solita.fi>
2013-04-23 21:48:22 +0300

    Barebones index page with Jade, see board with GET, add one stone to new board with POST.

--- src/main/scala/com/github/joonasrouhiainen/joogo/MainServlet.scala
+++ src/main/scala/com/github/joonasrouhiainen/joogo/MainServlet.scala
@@ -2,16 +2,24 @@ package com.github.joonasrouhiainen.joogo
 
 import org.scalatra._
 import scalate.ScalateSupport
+import scala.xml.XML
 
-class MainServlet extends JoogoStack {
+class MainServlet extends JoogoStack with ScalateSupport {
 
   get("/") {
-    <html>
-      <body>
-        <h1>Hello, world!</h1>
-        Say <a href="hello-scalate">hello to Scalate</a>.
-      </body>
-    </html>
+    contentType = "text/html"
+    jade("index", "board" -> new Board(9))
+  }
+
+  post("/") {
+    contentType = "text/html"
+
+    if (params.get("x").isDefined && params.get("y").isDefined) {
+      val x = params.get("x").get.toInt
+      val y = params.get("y").get.toInt
+      jade("index", "board" -> new Board(9).play(x, y))
+    }
+    jade("index", "board" -> new Board(9))
   }
   
 }
