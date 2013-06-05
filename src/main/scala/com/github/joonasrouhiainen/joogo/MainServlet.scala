b08f7e2cb37c2d128bf80e3cc069e3bf1733f8e9
Joonas Rouhiainen <Joonas.Rouhiainen@solita.fi>
2013-06-06 02:20:32 +0300

    Separate game creation and play on different pages, retrieve game by parameter instead of using a stateful controller.

--- src/main/scala/com/github/joonasrouhiainen/joogo/MainServlet.scala
+++ src/main/scala/com/github/joonasrouhiainen/joogo/MainServlet.scala
@@ -6,55 +6,61 @@ import com.github.joonasrouhiainen.joogo.data.RuntimeGameStorage
 class MainServlet extends JoogoStack {
 
   val storage = new RuntimeGameStorage
-  var id = ""
 
   newGame(9, 9)
 
-  def store(g: Game): Unit = {
-    id = storage.storeGame(g)
-  }
+  def store(g: Game): String = storage.storeGame(g)
+  def game(id: String): Option[Game] = storage.getGame(id)
 
-  def game: Game = storage.getGame(id).get
-
-  def newGame(x: Int, y: Int) = {
+  def newGame(x: Int, y: Int): String = {
     store(new Game(x, y).addPlayer(Black, new Player("black")).addPlayer(White, new Player("white")))
   }
 
-  def serveGame = {
+  def serveCreation() = {
     contentType = "text/html"
-    jade("index", "game" -> game)
+    jade("index")
   }
 
-  get("/") {
-    serveGame
+  def serveGame(id: String) = {
+    contentType = "text/html"
+    jade("game", "game" -> storage.getGame(id).get, "gameId" -> id)
+  }
+
+  get("/")  {
+    serveCreation
+  }
+
+  get("/g/:id") {
+    serveGame(params("id"))
+  }
+
+  post("/g/:id") {
+    if (params.get("x").isDefined && params.get("y").isDefined) {
+      val x = params.get("x").get.toInt
+      val y = params.get("y").get.toInt
+      val newId = store(game(params("id")).get.play(x, y))
+      serveGame(newId)
+    }
+    else serveGame(params("id"))
+  }
+
+  post("/g/:id/pass") {
+    store(game(params("id")).get.pass)
+    serveGame(params("id"))
+  }
+
+  post("/g/:id/resign") {
+    store(game(params("id")).get.resign)
+    serveGame(params("id"))
   }
 
   post("/") {
     if (params.get("x").isDefined && params.get("y").isDefined) {
-      val x = params.get("x").get.toInt
-      val y = params.get("y").get.toInt
-      store(game.play(x, y))
+      val x  = params("x").toInt
+      val y  = params("y").toInt
+      val id = newGame(x, y)
+      serveGame(id)
     }
-    serveGame
-  }
-
-  post("/pass") {
-    store(game.pass)
-    serveGame
-  }
-
-  post("/resign") {
-    store(game.resign)
-    serveGame
-  }
-
-  post("/new") {
-    if (params.get("x").isDefined && params.get("y").isDefined) {
-      val x = params.get("x").get.toInt
-      val y = params.get("y").get.toInt
-      newGame(x, y)
-    }
-    serveGame
   }
   
 }
